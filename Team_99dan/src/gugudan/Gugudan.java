64317bc4e1814a5ee20b74b4819c595732ba283b
jjangbg <khddhk123@naver.com>
2024-06-27 12:13:02 +0900

    1빠 김보근
    by 김보근

---
+++ Team_99dan/src/gugudan/Gugudan.java
@@ -0,0 +1,22 @@
+package gugudan;
+
+public class Gugudan {
+	public static void main(String[] args) {
+		
+		System.out.println("작성자: 김보근");
+		
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(2 + "*" + i + "=" + 2 * i);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
